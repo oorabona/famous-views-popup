e4720743c85cc4b3b893a1b7c95de5cef22260cd
Olivier ORABONA <olivier.orabona@gmail.com>
2014-11-06 05:47:38 +0100

    Definitely a bug. Trying to reproduce.

--- package.js
+++ package.js
@@ -16,14 +16,14 @@ Package.on_use(function(api) {
   api.use('reactive-var', 'client');
   api.use('blaze', 'client');
   api.imply('templating', 'client');
-  
+
   api.use('mjn:famous@0.3.0_5', 'client', { weak: true });
   api.use('raix:famono@0.9.14', { weak: true });
   api.use('gadicohen:famous-views@0.1.18', 'client');
 
   // Files to be added to project
-  api.add_files('popupmodal.html', 'client');
   api.add_files('popupmodal.coffee', 'client');
+  api.add_files('popupmodal.html', 'client');
 
   api.export('Popups', 'client');
 });
