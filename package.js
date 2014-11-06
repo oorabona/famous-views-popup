632c6f2b8ffbfd90bbc6b5133211b36b592aefa2
Olivier ORABONA <olivier.orabona@gmail.com>
2014-11-06 04:25:03 +0100

    Updated package.js with versions

--- package.js
+++ package.js
@@ -1,18 +1,17 @@
 Package.describe({
   name: 'oo:famous-views-popup',
   summary: 'Use Famo.us for your popups!',
-<<<<<<< HEAD
   version: '1.0.0',
-=======
-  version: '0.1.0',
->>>>>>> b3e5f335cc3a65ab932283dc7738ad05c056d393
   git: 'https://github.com/oorabona/famous-views-popup.git'
 });
+
 Package.onUse({
   // are as of meteor@0.9.0.
   api.versionsFrom('METEOR@0.9.0');
-  api.use('underscore', ['client']);
-  api.use('coffeescript', ['client']);
-  api.use('gadicohen:famous-views', 'client');
+  // FIXME: Should I remove coffeescript dependency here?
+  api.use(['underscore','coffeescript'], 'client');
+  api.use('gadicohen:famous-views@0.1.20', 'client');
+
+  // Files to be added to project
   api.addFiles(['popupmodal.coffee', 'popupmodal.html'], 'client');
 });
