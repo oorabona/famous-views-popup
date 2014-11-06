b3e5f335cc3a65ab932283dc7738ad05c056d393
Olivier ORABONA <olivier.orabona@gmail.com>
2014-11-06 03:41:33 +0100

    Initial commit

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'oo:famous-views-popup',
+  summary: 'Use Famo.us for your popups!',
+  version: '0.1.0',
+  git: 'https://github.com/oorabona/famous-views-popup.git'
+});
+Package.onUse({
+  // are as of meteor@0.9.0.
+  api.versionsFrom('METEOR@0.9.0');
+  api.use('underscore', ['client']);
+  api.use('coffeescript', ['client']);
+  api.use('gadicohen:famous-views', 'client');
+  api.addFiles(['popupmodal.coffee', 'popupmodal.html'], 'client');
+});
