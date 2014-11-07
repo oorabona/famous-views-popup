5be8144782566dd7b5ddb36d594eb110ee56bb74
Olivier ORABONA <olivier.orabona@gmail.com>
2014-11-07 04:53:14 +0100

    Version bump 1.0.1

--- package.js
+++ package.js
@@ -1,31 +1,29 @@
 Package.describe({
   name: 'oorabona:famous-views-popup',
   summary: 'Use Famo.us for your popups!',
-  version: '0.1.3',
+  version: '1.0.1',
   git: 'https://github.com/oorabona/famous-views-popup.git'
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
   // Meteor version
   api.versionsFrom('METEOR@1.0');
-  // FIXME: Should I remove coffeescript dependency here?
   api.use('coffeescript@1.0.1', 'client');
 
   // Core dependencies
   api.use('underscore@1.0.1', 'client');
   api.use('reactive-var', 'client');
-  api.use('blaze@2.0.0', 'client');
-  api.use('templating@1.0.5', 'client');
-  api.use('tracker', 'client');
+  api.use('templating', 'client');
   api.use('random', 'client');
 
   api.use('mjn:famous@0.3.0_5', 'client', { weak: true });
   api.use('raix:famono@0.9.14', { weak: true });
 
-  api.use('gadicohen:famous-views', 'client');
-  api.imply('gadicohen:famous-views@0.1.18')
+  api.use('gadicohen:famous-views@0.1.20', 'client');
 
   // Files to be added to project
-  api.add_files('popupmodal.html', 'client');
-  api.add_files('popupmodal.coffee', 'client');
+  api.addFiles('popupmodal.html', 'client');
+  api.addFiles('popupmodal.coffee', 'client');
+  api.addFiles('popupmodal.css', 'client');
+  //api.export('Popups');
 });
