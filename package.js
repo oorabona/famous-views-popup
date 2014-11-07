2ef57e679a6f8e146e030ad6f421d07271d1f71e
Olivier ORABONA <olivier.orabona@gmail.com>
2014-11-07 17:19:00 +0100

    Version bump 1.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'oorabona:famous-views-popup',
   summary: 'Use Famo.us for your popups!',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/oorabona/famous-views-popup.git'
 });
 
